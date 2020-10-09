05fef30d1eba777070150055493131a26ae66b18
tvqphuoc01 <tvqphuoc.01@gmail.com>
2020-10-09 13:57:39 +0700

    first commit

---
+++ index/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/img', express.static('img'));
+app.use(express.static('css'));
+app.use('/font', express.static('font'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
